48c1d43480ea94f5acfcc55ed5ea8cf8fbd8aa39
ziggybuilds <ellisjoshuag@gmail.com>
2017-10-02 19:54:05 -0400

    Push test

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 module.exports = function(bb8) {
-	bb8.respond(/TEST-BB8-JSR/, function(res) {
-	  return res.send("I am alive!");
+	bb8.respond(/Please welcome (.*) and (.*)/, function(res) {
+		var name1;
+		var name2;
+		naem1 = msg.match[1];
+		naem2 = msg.match[2];
+	  return res.send("Welcome " + name1 + " and " + name2 + ". Have a glorious day");
 	});
 
-	bb8.hear(/TESTBB8/, function(res) {
-	  return res.send("I am alive!");
-	});
+
 }
