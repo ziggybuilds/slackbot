65fcb77fc7c88cb6e6a5a69f4c8075577a78c1bc
ziggybuilds <ellisjoshuag@gmail.com>
2017-10-02 19:56:01 -0400

    Push test

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ module.exports = function(bb8) {
 	bb8.respond(/Please welcome (.*) and (.*)/, function(res) {
 		var name1;
 		var name2;
-		naem1 = msg.match[1];
-		naem2 = msg.match[2];
+		name1 = msg.match[1];
+		name2 = msg.match[2];
 	  return res.send("Welcome " + name1 + " and " + name2 + ". Have a glorious day");
 	});
 
